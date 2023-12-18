10075eacd9db83b094a63ab7c914f8ce9b32af3b
rn-ak <63904518+rn-ak@users.noreply.github.com>
2023-12-18 17:01:26 +0900

    Delete EDA 프로젝트_3팀_2조 directory

--- EDA 프로젝트_3팀_2조/1.단일 열 분석/1.카테고리 열 분석/2. 카테고리별 count 값 비교.sql
+++
@@ -1,5 +0,0 @@
--- 카테고리별 count
-select category, count(period) as count
-from datatable
-group by 1 
-order by 1
--- EDA 프로젝트_3팀_2조/2.다중 열 상관관계 분석/2.카테고리 - gender/1-1. 정확한 비교를 위한 Count 데이터.sql
+++
@@ -1,7 +0,0 @@
-SELECT gender, ROUND(AVG(ratio)) AS AVG_RATIO , count(*) as count
-FROM datatable
-WHERE gender IS NOT null
-GROUP BY genderSELECT gender, ROUND(AVG(ratio)) AS AVG_RATIO 
-FROM datatable
-WHERE gender IS NOT null
-GROUP BY gender
